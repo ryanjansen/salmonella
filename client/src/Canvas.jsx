4a1814e98a1f0f2e434ca7ac7ab21f13d04ebb87
CSA100 <67359421+Snail664@users.noreply.github.com>
2022-01-07 15:50:37 +0800

    panning

--- client/src/Canvas.jsx
+++ client/src/Canvas.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
-import Draw from './components/Draw';
+import React from "react";
+import { Stage, Layer, Text } from "react-konva";
+
+const scaleBy = 1.01;
 
 function Canvas({ components }) {
-
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>{Object.entries(components).map(([key, value]) => {
-        switch (value.type) {
-          case "Text":
-            return <Text
-              key={key}
-              text={value.text}
-              x={value.x}
-              y={value.y}
-            />
-          default:
-            return <Text
-            text="Wrong"
-            />
-        }
-
-      })}</Layer>
+    <Stage width={window.innerWidth} height={window.innerHeight} draggable>
+      <Layer>
+        {Object.entries(components).map(([key, value]) => {
+          switch (value.type) {
+            case "Text":
+              return (
+                <Text key={key} text={value.text} x={value.x} y={value.y} />
+              );
+            default:
+              return <Text text="Wrong" />;
+          }
+        })}
+      </Layer>
     </Stage>
   );
 }
