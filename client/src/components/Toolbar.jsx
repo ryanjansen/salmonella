4aba5741588b010a03519cc1b214e6b67e9344d5
CSA100 <67359421+Snail664@users.noreply.github.com>
2022-01-07 19:04:10 +0800

    set up tailwind

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Toolbar({ tools, setComponents }) {
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0 }}>
+    <div
+      style={{ position: "fixed", top: 0, left: 0 }}
+      className="text-3xl font-bold underline"
+    >
       {tools.map((tool, index) => (
         <div
           key={index}
