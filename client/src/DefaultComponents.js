ce9523064ae7a2f5f305ac3687203d440c648c64
Ryan Jansen <rjgiga444@gmail.com>
2022-01-07 16:47:58 +0800

    Deleted text componenets

--- client/src/DefaultComponents.js
+++ client/src/DefaultComponents.js
@@ -1,14 +1,16 @@
 export const defaultText = {
-  type: "text",
-  text: "Text",
+  type: 'text',
+  text: 'Text',
   x: 50,
   y: 100,
-  fontFamily: "Callibri",
-  fontSize: 28
-}
+  fontFamily: 'Callibri',
+  fontSize: 28,
+};
 
 export const defaultEquation = {
-  type: "equation",
-  latex: "x = y",
-  fontSize: 28
-}
+  type: 'equation',
+  latex: 'x = y',
+  x: 20,
+  y: 40,
+  fontSize: 28,
+};
