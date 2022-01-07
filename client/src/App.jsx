f3aa48e1a78a5dadc28661fa8ef203eb28f40646
Ryan Jansen <rjgiga444@gmail.com>
2022-01-07 11:05:07 +0800

    Created canvas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import { Stage } from 'react-konva';
-import Konva from 'konva';
-import './App.css';
+import Canvas from './Canvas';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and die to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> years.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <Canvas />;
 }
 
 export default App;
