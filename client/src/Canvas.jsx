75fbe0a2b2323bb5609dc5ce5b079341dcf7753c
Ryan Jansen <rjgiga444@gmail.com>
2022-01-07 16:46:28 +0800

    added create component function

--- client/src/Canvas.jsx
+++ client/src/Canvas.jsx
@@ -129,7 +129,7 @@ function Canvas({ components, setComponents, setSelected }) {
       <Layer>
         {Object.entries(components).map(([key, value]) => {
           switch (value.type) {
-            case "Text":
+            case "text":
               return (
                 <Text
                   // Mandatory - Key, Draggable
@@ -170,9 +170,9 @@ function Canvas({ components, setComponents, setSelected }) {
                   fill={value.fill != null ? value.fill : "black"}
                 />
               );
-            case "Equation":
+            case "equation":
               return <Text text="Wrong" />;
-            case "Image":
+            case "image":
               break;
           }
         })}
