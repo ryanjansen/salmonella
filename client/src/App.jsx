cec7a6a6ff3b7c2250a89fabba4ffca30d5694d3
CSA100 <67359421+Snail664@users.noreply.github.com>
2022-01-07 16:46:24 +0800

    cleaning up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import Canvas from './Canvas';
-import Draw from './components/Draw';
-import Toolbar from './components/Toolbar';
-import { Editor } from './components/Editor';
+import React, { useState, useEffect } from "react";
+import Canvas from "./Canvas";
+import Toolbar from "./components/Toolbar";
+import { Editor } from "./components/Editor";
 
 function App() {
   const [tools, setTools] = useState([
-    { icon: 'Draw', component: <Draw /> },
-    { icon: 1, component: <Draw /> },
-    { icon: 2, component: <Draw /> },
+    { icon: "Draw", component: "Text" },
+    { icon: 1, component: "Text" },
+    { icon: 2, component: "Text" },
   ]);
   const [selected, setSelected] = useState(-1);
   const [components, setComponents] = useState({
     1: {
-      type: 'Text',
-      text: 'First',
+      type: "Text",
+      text: "First",
       x: 50,
       y: 100,
-      fontFamily: 'Calibri',
+      fontFamily: "Calibri",
       fontSize: 28,
     },
     2: {
-      type: 'Text',
-      text: 'testing',
+      type: "Text",
+      text: "testing",
       x: 150,
       y: 200,
-      textDecoration: 'underline',
+      textDecoration: "underline",
     },
     3: {
-      type: 'Equation',
+      type: "Equation",
     },
   });
-
-  console.log('hi');
-
-  console.log('selected tools: ', components);
 
   const getAttribute = (attribute) => {
     return components[selected][attribute];
